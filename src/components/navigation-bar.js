1ea8ae8422cb39a7756784f30dd0a5f9729d0b1a
Glitch (react-header-bar-starting-point) <none>
2018-04-09 09:42:46 -0700

    first drill done

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -4,3 +4,31 @@ import './navigation-bar.css';
 
 // The NavigationBar component goes here.  It should be the default export.
 
+export default function NavigationBar(props){
+    // const links = [{
+    //     text: 'Courses',
+    //     href: 'http://www.thinkful.com/courses/'
+    // }, {
+    //     text: 'Mentorship',
+    //     href: 'http://www.thinkful.com/mentorship/'
+    // }];
+    
+ 
+    const links = props.links.map(link => 
+    <li>
+        <a href={link.href}>
+        {link.text}
+        </a>
+    </li>
+    )
+ return (
+     <div className = "navigation-bar">
+         <h1>{props.title}</h1>
+   <nav>      
+   <ul className = "navigation-bar-nav">
+         {links}
+         </ul>
+   </nav>
+     </div>
+ )
+}
